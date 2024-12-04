4433d081d8b1c2033aa9b12ac3860ee268460013
oya <seikasu9@gmail.com>
2024-12-04 13:13:04 +0900

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloGitHubEnterprise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub Enterprise!");
+        }
+    }
+}
